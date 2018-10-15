860feda7dfec3abfab7e307259246cae30334bbe
Matt Gasper <mrgasper27@gmail.com>
2018-10-15 12:43:37 -0500

    Added movie-search

--- liri.js
+++ liri.js
@@ -25,6 +25,23 @@ if (command == "spotify-this-song") {
 if (command == "movie-this") {
     let movieName = searchTerm;
     // Return movie title, release year, IMDB rating, RT rating, Country where it was produced, language of movie, movie plot summary, actors in the movie
+    let queryURL = "https://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    request(queryURL), function(error, response, body){
+    if (!error && response.statusCode === 200) {
+        console.log(`
+        Title: ${JSON.parse(body).Title}
+        Release Year: ${JSON.parse(body).Year}
+        IMDB Rating: ${JSON.parse(body).imdbRating}
+        RottenTomatoes Rating: ${JSON.parse(body).Ratings[1].Value}
+        Country: ${JSON.parse(body).Country}
+        Language: ${JSON.parse(body).Language}
+        Plot Summary: ${JSON.parse(body).Plot}
+        Actors: ${JSON.parse(body).Actors}
+        =============================
+        `)
+    }
+    }
 }
 
 // 4th Search Term
